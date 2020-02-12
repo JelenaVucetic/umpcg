e08b923dbbb91f790e67d8b93657eed8fad5e3ed
Darko Vucetic <darko.vucetic7@gmail.com>
2020-02-12 20:21:11 +0100

    mobile nav

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -9,7 +9,7 @@
         </div>
         <a href="/about" style="text-decoration: none;">
             <div class="aboutUs">
-                <img src="/img/O nama - ikonica.svg" alt="O nama" style="padding-right: 10px;">
+                <img src="/img/O nama - ikonica.svg" alt="O nama" id="aboutIco">
                 <p>O NAMA</p>
             </div>
         </a>     
@@ -17,51 +17,42 @@
 </nav>
 
 <nav class="navbar">
+<div class='mobile'>
+        <form action="">
+            <input type="text" placeholder="Search...">
+            <div class="mobileSearch"></div>
+        </form> 
+        </div>
     <div class="container-fluid" id="mySecondNav">
-
-
-        <div class="bottomNav">
+            <div>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <div class="desctop">
             <div style="display:flex;">
-                <div>
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
                 <div class="search-container">
-                        <form action="">
-                        <input type="text" name="search">
+                    <form action="">
                         <button type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                </div>
-                <div id="mobileSearch">
-                    <form>
-                        <input type="text" name="search" placeholder="Search.." id="searchBar">
+                        <input type="text" name="search">
                     </form>
                 </div>
-                
-        </div>
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                &nbsp;
-            </ul>
-
+            </div>
             <ul class="nav navbar-nav pages" id="navPages">
               <li><a href="">Članovi</a></li>
               <li><a href="">Projekti & Aktivnosti</a></li>
               <li><a href="">E-Books</a></li>
-            </ul>
-        </div>
-        <div> <!-- Right Side Of Navbar -->
+            </ul> 
             <ul class="nav navbar-nav navbar-right socialMedia">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                <li><a href=""> <img src="/img/facebook.svg" alt="" style="width:20px;"> </a></li>
-              <li><a href=""> <img src="/img/instagram.svg" alt="" style="width:20px;"> </a></li>
-              <li><a href=""> <img src="/img/twitter.svg" alt="" style="width:20px;"> </a></li>
+                    <li><a href=""> <img src="/img/facebook.svg" alt="" style="width:20px;"> </a></li>
+                    <li><a href=""> <img src="/img/instagram.svg" alt="" style="width:20px;"> </a></li>
+                    <li><a href=""> <img src="/img/twitter.svg" alt="" style="width:20px;"> </a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -73,7 +64,7 @@
                             <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">
+                                            document.getElementById('logout-form').submit();">
                                     Logout
                                 </a>
 
@@ -85,7 +76,7 @@
                     </li>
                 @endif
             </ul>
-        </div>
+            </div>
         </div>
     </div>
 </nav>
